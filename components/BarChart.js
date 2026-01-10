6869ba9e484d4fdfcae1b8b3193a29833b850561
SAMAJU76 <shantanu.nandimajumdar@gmail.com>
2026-01-10 08:04:21 +0800

    Create BarChart.js

---
+++ components/BarChart.js
@@ -0,0 +1,18 @@
+
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+export default function BarChart({ data }) {
+  const chartData = {
+    labels: data.map(item => item.name),
+    datasets: [
+      {
+        label: 'Spend',
+        data: data.map(item => item.value),
+        backgroundColor: '#3b82f6',
+      },
+    ],
+  };
+  return <Bar data={chartData} />;
+}
